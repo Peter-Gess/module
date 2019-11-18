68c3caf2b70e03f2890df5d180ca5e7ded9fc76a
Peter-Gess <gesspeter@gmail.com>
2019-11-17 19:26:49 -0600

    Create region.tf

---
+++ region.tf
@@ -0,0 +1,6 @@
+//--------------------------------------------------------------------
+// Modules
+module "multi_region" {
+  source  = "app.terraform.io/tf-enablement/multi-region/aws"
+  version = "1.1.0"
+}
